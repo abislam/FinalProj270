5d5ee54d34cd9acb444404f0573fda40cfbc35a9
Abed Islam <aislam360@gmail.com>
2019-12-05 16:10:32 -0500

    Shell complete

--- FinalProject.java
+++ FinalProject.java
@@ -23,3 +23,62 @@ User Menu:
 2)Place order(or purchase item)
 Implement these options for user activities.
 */
+
+import java.util.Scanner;
+
+public class FinalProject{
+
+	private Scanner input = new Scanner(System.in);
+	private Item[] items = new Item[100];
+
+	public static void main(String[] args){
+		FinalProject fp = new FinalProject();
+	}
+
+	public void mainMenu(){
+		//display menu and ask for user selection (Validate)
+	}
+
+	public void adminMenu(){
+		////display menu and ask for user selection (Validate)
+	}
+
+	public void userMenu(){
+		//display menu and ask for user selection (Validate)
+	}
+
+	private void displayAllItems(){
+
+	}
+
+	public int searchForItem(){
+
+	}
+
+	private void deleteItem(int index){
+
+	}
+
+	private void updateItem(int index){
+
+	}
+
+	private void addItem(){
+
+	}
+
+	private void purchaseItem(int index){
+
+	}
+}
+
+class Item {
+
+	private String id;
+	private String name;
+	private double price;
+	private int quantity;
+
+	//add constructors
+	//add get and set methods
+}
