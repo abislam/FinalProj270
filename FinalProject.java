12596883b7126ad1022d5e489ef03abb4685e786
Abed <aislam360@gmail.com>
2019-12-05 04:12:27 -0500

    Initial commit

---
+++ FinalProject.java
@@ -0,0 +1,25 @@
+//initial file
+/*
+Objective: Create an InventoryManagementSystem(IMS).The IMS will contain a catalog of items(stationery Items)that can be managed, viewed, and purchased. 
+
+
+
+Command line Menu:
+Main Menu:
+1)Admin
+2)User
+3)Exit
+User could select an option from this list. If user select 1, it should display Admin Menu with the following options:
+
+Admin Menu:
+1)Add new item
+2)Search and update item
+3)Search and delete
+Implements these options for admin personnel.In main menu, if user select 2 from the Main Menu it should display user menu. 
+
+User menu should contains the following options:
+User Menu:
+1)Search item
+2)Place order(or purchase item)
+Implement these options for user activities.
+*/
